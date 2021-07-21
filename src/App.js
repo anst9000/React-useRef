77005fb8003dfbcb7b7ebaa958aeef15c1e3d7de
Anders Strömberg <40022844+anst9000@users.noreply.github.com>
2021-07-21 12:53:59 +0200

    Finish example with useRef Worked in App.js and showed 3 different cases of using useRef() hook in React.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const inputRef = useRef();
+  const prevName = useRef("");
+
+  /*
+  const renderCount = useRef(1);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
+*/
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      {/* <div>My name is {name}</div> */}
+      <div>
+        My name is {name} and it used to be {prevName.current}
+      </div>
+      {/* <div>I rendered {renderCount.current} times</div> */}
+      {/* <button onClick={focus}>Focus</button> */}
+    </>
   );
 }
